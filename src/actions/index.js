93b46ef365ddf3c025112860b57f6b9dd1e6f637
Maxwell Cady <maxwell.josef@gmail.com>
2017-02-27 16:39:54 -0800

    create ability to search for cards

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,8 @@ export const FETCH_CARDS = "FETCH_CARDS";
 
 const ROOT_URL = "https://api.magicthegathering.io/v1/cards";
 
-export function fetchCards() {
-  const request = axios.get(`${ROOT_URL}?name=nissa`);
+export function fetchCards(inputtedCard) {
+  const request = axios.get(`${ROOT_URL}?name=${inputtedCard}`);
 
   return (dispatch) => {
     request.then(({data}) => {
--- src/components/deck_new.js
+++ src/components/deck_new.js
@@ -7,6 +7,13 @@ import { connect } from 'react-redux';
 
 
 class NewDeck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {inputtedCard: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   // Used for routing after submit
   static contextTypes = {
@@ -16,21 +23,41 @@ class NewDeck extends Component {
   renderCards() {
     return this.props.foundCards.map((card) => {
       return(
-        <li>
+        <li key={card.id}>
           {card.name}
         </li>
       );
     });
   }
 
+  handleChange(event) {
+    this.setState({inputtedCard: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.props.fetchCards(this.state.inputtedCard)
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <div>New Deck Component!</div>
-        <span onClick={this.props.fetchCards}>Get Nissas</span>
+
+        <form onSubmit={this.handleSubmit}>
+
+          <label>Card name</label>
+
+          <input type="text" value={this.state.inputtedCard} onChange={this.handleChange} />
+
+          <button type="submit">Get Cards</button>
+
+        </form>
+
         <ul>
           <div>{this.renderCards()}</div>
         </ul>
+
       </div>
     );
   }
