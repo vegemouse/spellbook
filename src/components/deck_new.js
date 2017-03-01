112e9e447eddd2f4f712e6470d0352126dffd748
Maxwell Cady <maxwell.josef@gmail.com>
2017-03-01 14:33:27 -0800

    can add cards to deck and sideboard. can name deck

--- src/components/deck_new.js
+++ src/components/deck_new.js
@@ -13,10 +13,14 @@ class NewDeck extends Component {
       inputtedCard: '',
       searchSubmitted: false,
       activeCard: null,
+      mainDeckArray: [],
+      sideboardArray: [],
+      deckName: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
   }
 
   // Used for routing after submit
@@ -38,8 +42,9 @@ class NewDeck extends Component {
     return sortedCards.map((card) => {
       if (card.imageUrl) {
         return(
-          <li key={card.id}>
-            <span onClick={this.setActiveCard(card)}><strong>{card.name}</strong> - {card.types[0]} / {card.cmc} CMC</span>
+          <li key={card.id} onClick={this.setActiveCard(card)}>
+            <strong>{card.name}</strong>
+            <span className="pull-right">{card.types[0]} / {card.cmc} CMC</span>
           </li>
         );
       }
@@ -50,17 +55,78 @@ class NewDeck extends Component {
     var activeCard = this.state.activeCard;
     if (activeCard !== null) {
       return (
-        <div className="new-deck-selected col-lg-3">
+        <div className="new-deck-selected">
           <img src={activeCard.imageUrl} alt={activeCard.name} />
           <div className="new-deck-selected-details">
             <strong>{activeCard.name}</strong>
-            <span>{activeCard.cmc}</span>
+            <span className="pull-right">{activeCard.cmc}</span>
             <div>{activeCard.type}</div>
             <div>{activeCard.text}</div>
+            <button onClick={this.addCard(activeCard)}>+1</button>
+            <button onClick={this.addFour(activeCard)}>+4</button>
+            <button className="pull-right" onClick={this.addSideboard(activeCard)}>+SB</button>
           </div>
         </div>
       );
     }
+  }
+
+  renderDeck() {
+    var sortedCards = this.state.mainDeckArray.sort(function(a, b) {
+      var textA = a.name.toUpperCase();
+      var textB = b.name.toUpperCase();
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+
+    return sortedCards.map((card) => {
+      if (card.imageUrl) {
+        return(
+          <div>
+            <li key={card.id}>
+              <span onClick={this.setActiveCard(card)}><strong>{card.name}</strong> - {card.types[0]} / {card.cmc} CMC</span>
+            </li>
+          </div>
+        );
+      }
+    });
+  }
+
+  renderSideboard() {
+    var sortedCards = this.state.sideboardArray.sort(function(a, b) {
+      var textA = a.name.toUpperCase();
+      var textB = b.name.toUpperCase();
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+
+    return sortedCards.map((card) => {
+      if (card.imageUrl) {
+        return(
+          <div>
+            <li key={card.id}>
+              <span onClick={this.setActiveCard(card)}><strong>{card.name}</strong> - {card.types[0]} / {card.cmc} CMC</span>
+            </li>
+          </div>
+        );
+      }
+    });
+  }
+
+  addCard(card) {
+    return function() {
+      this.setState({mainDeckArray: this.state.mainDeckArray.concat([card])});
+    }.bind(this);
+  }
+
+  addFour(card) {
+    return function() {
+      this.setState({mainDeckArray: this.state.mainDeckArray.concat([card], [card], [card], [card])});
+    }.bind(this);
+  }
+
+  addSideboard(card) {
+    return function() {
+      this.setState({sideboardArray: this.state.sideboardArray.concat([card])});
+    }.bind(this);
   }
 
   handleChange(event) {
@@ -76,28 +142,47 @@ class NewDeck extends Component {
   }
 
   setActiveCard(card){
-      return function() {
-          this.setState({activeCard: card});
-      }.bind(this);
+    return function() {
+        this.setState({activeCard: card});
+    }.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({deckName: event.target.value});
   }
 
   render() {
     return (
       <div className="container new-deck">
         <h2>Build a New Deck</h2>
-          <div className="new-deck-search col-lg-5">
-            <form onSubmit={this.handleSubmit}>
-              <h4>Search Card Name</h4>
-              <input type="text" value={this.state.inputtedCard} onChange={this.handleChange} />
-              <button type="submit">Search</button>
-            </form>
-            <ul>
-              {this.renderResults()}
-            </ul>
-          </div>
+        <div>
+          <label>Deck Name</label><input type="text" value={this.state.deckName} onChange={this.handleNameChange} />
+        </div>
 
-            {this.renderActiveCard()}
+        <div className="new-deck-search col-md-5">
+          <form onSubmit={this.handleSubmit}>
+            <h4>Search Card Name</h4>
+            <input type="text" value={this.state.inputtedCard} onChange={this.handleChange} />
+            <button type="submit">Search</button>
+          </form>
+          <ul>
+            {this.renderResults()}
+          </ul>
+        </div>
 
+        <div className="col-md-3">
+          {this.renderActiveCard()}
+        </div>
+
+        <div className="col-md-4">
+          <strong>Deck Name</strong><span className="pull-right">{this.state.mainDeckArray.length} cards</span>
+          <br />
+          <strong>Main Deck</strong>
+          {this.renderDeck()}
+          <br />
+          <strong>Sideboard</strong>
+          {this.renderSideboard()}
+        </div>
       </div>
     );
   }
