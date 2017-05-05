d0d38247ff454f2aeea4abe7a031ca333dfc2b68
Maxwell Cady <maxwell.josef@gmail.com>
2017-05-05 15:46:14 -0700

    add media queries for header buttons

--- src/components/header.js
+++ src/components/header.js
@@ -10,6 +10,10 @@ export default class Header extends Component {
           <Link className="build" to="/decks/new">Build a Deck</Link>
           <Link className="all" to="/decks">View Decks</Link>
         </div>
+        <div className="header_buttons_newdeck_small">
+          <Link className="build" to="/decks/new"><i className="fa fa-plus" aria-hidden="true"></i></Link>
+          <Link className="all" to="/decks"><i className="fa fa-eye" aria-hidden="true"></i></Link>
+        </div>
       </header>
     );
   }
